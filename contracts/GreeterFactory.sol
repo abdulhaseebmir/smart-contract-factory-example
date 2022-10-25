e4482d37397affac4f38da1b539704653bb9f703
abduhaseebmir <mabdulhaseebq@gmail.com>
2022-10-25 17:07:06 +0500

    GreeterFactory updated

--- contracts/GreeterFactory.sol
+++ contracts/GreeterFactory.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract GreeterFactory {
+import "./Greeter.sol";
 
+contract GreeterFactory {
+    Greeter[] public GreeterArray;
+
+    function CreateNewGreeter(string memory _greeting) public {
+        Greeter greeter = new Greeter(_greeting);
+        GreeterArray.push(greeter);
+    }
+
+    function greetingGetter(uint256 _greeterIndex) public view returns(string memory) {
+        return GreeterArray[_greeterIndex].greet();
+    }
+
+    function greetingSetter(uint256 _greeterIndex, string memory _greeting) public {
+        GreeterArray[_greeterIndex].setGreeting(_greeting);
+    }
 }
