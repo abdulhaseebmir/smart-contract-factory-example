3c6d9f8bbe1d1f6362ae2760b87282d3727ced8b
abduhaseebmir <mabdulhaseebq@gmail.com>
2022-10-25 17:06:02 +0500

    Greeter smart contractgetter and setter

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,5 +2,21 @@
 pragma solidity ^0.8.0;
 
 contract Greeter {
+    // state variable greeting
+    string private greeting;
 
+    constructor(string memory _greeting) {
+        // setting value of greeting at contract creation
+        greeting = _greeting;
+    }
+
+    // function returns the value stored in greeting variable
+    function greet() public view returns(string memory) {
+        return greeting;
+    }
+
+    // function sets/updates the value of greeting variable
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
 }
